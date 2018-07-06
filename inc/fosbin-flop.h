4b5e7e34635f5be370da649a1888993ccb81644d
Derrick McKee <derrick.mckee@gmail.com>
2018-07-06 15:21:18 -0400

    getting build to work

---
+++ inc/fosbin-flop.h
@@ -0,0 +1,10 @@
+//
+// Created by derrick on 7/6/18.
+//
+
+#ifndef FOSBIN_FLOP_FOSBIN_FLOP_H
+#define FOSBIN_FLOP_FOSBIN_FLOP_H
+
+#include "fosbin-flop-config.h"
+
+#endif //FOSBIN_FLOP_FOSBIN_FLOP_H
---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "fosbin-flop.h"
+
+int main(int argc, char **argv) {
+    std::cout << EXE_NAME << " v. " << VERSION_MAJOR << "." << VERSION_MINOR << std::endl;
+}
