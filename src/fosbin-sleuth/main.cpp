bcd5deb9e2cc20ba6835b39115b21818841d960e
Derrick McKee <derrick.mckee@gmail.com>
2018-09-26 11:19:15 -0400

    adding in proper command line argument parsing

--- src/fosbin-sleuth/main.cpp
+++ src/fosbin-sleuth/main.cpp
@@ -5,23 +5,49 @@
 #include "argumentTestCase.h"
 #include "binaryDescriptor.h"
 
-void usage() {
-    std::cout << "fosbin-sleuth /path/to/binary/descriptor" << std::endl;
-}
+#include <boost/program_options.hpp>
 
 int main(int argc, char **argv) {
-    std::cout << SLEUTH_NAME
-              << " v. " << FOSBIN_VERSION_MAJOR << "." << FOSBIN_VERSION_MINOR
-              << " starting. Good luck, Sherlock." << std::endl;
+    uint32_t thread_count;
 
-    if (argc != 2) {
-        usage();
+    namespace po = boost::program_options;
+
+    po::options_description generic("Generic options");
+    generic.add_options()
+            ("version,v", "Prints version string")
+            ("help,h", "Prints this message")
+            ("num-threads,t",
+             po::value<uint32_t>(&thread_count)->default_value(std::thread::hardware_concurrency()),
+             "Number of threads to use")
+            ("binary-desc,i", po::value<fs::path>()->required(), "/path/to/binary/descriptor");
+
+    po::positional_options_description p;
+    p.add("binary-desc", -1);
+
+    po::variables_map vm;
+    try {
+        po::store(po::command_line_parser(argc, argv)
+                          .options(generic)
+                          .positional(p)
+                          .run(), vm);
+        po::notify(vm);
+    } catch (const po::error &e) {
+        std::cout << generic << std::endl;
         exit(1);
     }
 
-    fbf::BinaryDescriptor binDesc(argv[1]);
+    if (vm.count("version")) {
+        std::cout << SLEUTH_NAME
+                  << " v. " << FOSBIN_VERSION_MAJOR << "." << FOSBIN_VERSION_MINOR << std::endl;
+        exit(0);
+    }
 
-    fbf::FullSleuthTest test(argv[1], STR_LEN, PTR_LEN, std::thread::hardware_concurrency());
+    if (vm.count("help")) {
+        std::cout << generic << std::endl;
+        exit(0);
+    }
+
+    fbf::FullSleuthTest test(vm["binary-desc"].as<fs::path>(), STR_LEN, PTR_LEN, thread_count);
     test.run();
     test.output(std::cout);
 
