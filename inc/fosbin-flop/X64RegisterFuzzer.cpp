9025e132bffc3a465e506c4ec257518e5d520183
Derrick McKee <derrick.mckee@gmail.com>
2018-12-02 10:34:40 -0500

    starting X86 fuzzer

---
+++ inc/fosbin-flop/X64RegisterFuzzer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by derrick on 11/30/18.
+//
+
+#include "X64RegisterFuzzer.h"
---
+++ inc/fosbin-flop/X64RegisterFuzzer.h
@@ -0,0 +1,15 @@
+//
+// Created by derrick on 11/30/18.
+//
+
+#ifndef FOSBIN_X64REGISTERFUZZER_H
+#define FOSBIN_X64REGISTERFUZZER_H
+
+namespace fbf {
+    class X64RegisterFuzzer {
+
+    };
+}
+
+
+#endif //FOSBIN_X64REGISTERFUZZER_H
