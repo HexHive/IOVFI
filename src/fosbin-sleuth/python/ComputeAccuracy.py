426edd7275da0224acfd577ab9dfad8d8c572b50
Derrick McKee <derrick.mckee@gmail.com>
2019-01-30 12:33:36 -0500

    fixing bugs

--- src/fosbin-sleuth/python/ComputeAccuracy.py
+++ src/fosbin-sleuth/python/ComputeAccuracy.py
@@ -23,28 +23,29 @@ def main():
     failed_guesses = list()
     unknown_guesses = list()
 
-    for guess_path in args.guesses:
-        pathlist = Path(guess_path).glob("**/*.bin")
-        for path in pathlist:
-            path = os.path.abspath(path)
-            with open(path, "rb") as guessfile:
-                guessmap = pickle.load(guessfile)
-                for key, guess_idx in guessmap.items():
-                    if guess_idx == FBDecisionTree.UNKNOWN_FUNC:
-                        unknown_guesses.append(key)
-                        continue
+    for guess_path_list in args.guesses:
+        for guess_path in guess_path_list:
+            pathlist = Path(guess_path).glob("**/*.bin")
+            for path in pathlist:
+                path = os.path.abspath(path)
+                with open(path, "rb") as guessfile:
+                    guessmap = pickle.load(guessfile)
+                    for key, guess_idx in guessmap.items():
+                        if guess_idx == FBDecisionTree.UNKNOWN_FUNC:
+                            unknown_guesses.append(key)
+                            continue
 
-                    guesses = tree.get_equiv_classes(guess_idx)
-                    found = False
-                    for guess in guesses:
-                        guess = guess[len(args.prefix):]
-                        if key.find(guess) >= 0:
-                            successful_guesses.append(key)
-                            found = True
-                            break
+                        guesses = tree.get_equiv_classes(guess_idx)
+                        found = False
+                        for guess in guesses:
+                            guess = guess[len(args.prefix):]
+                            if key.find(guess) >= 0:
+                                successful_guesses.append(key)
+                                found = True
+                                break
 
-                    if not found:
-                        failed_guesses.append(key)
+                        if not found:
+                            failed_guesses.append(key)
 
     print("Successful guesses:  {} ({})".format(len(successful_guesses), len(successful_guesses) / (
                 len(successful_guesses) + len(failed_guesses))))
