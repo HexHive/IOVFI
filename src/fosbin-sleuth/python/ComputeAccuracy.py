c7d74eb8c7b99a924b6d80643199a52566a5798f
Derrick McKee <derrick.mckee@gmail.com>
2019-03-15 11:00:25 -0400

    correcting false negative calculation

--- src/fosbin-sleuth/python/ComputeAccuracy.py
+++ src/fosbin-sleuth/python/ComputeAccuracy.py
@@ -63,6 +63,7 @@ def main():
                             bad_guesses.add(str(equiv_class))
                         # print("{}: {}\n".format(func_desc.name, " ".join(bad_guesses)))
                         false_pos.append(func_desc)
+                        false_neg.append(func_desc)
 
             precision = len(true_pos) / (len(true_pos) + len(false_pos))
             recall = len(true_pos) / (len(true_pos) + len(false_neg))
