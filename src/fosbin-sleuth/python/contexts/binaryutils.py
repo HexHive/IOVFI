981ae3d6e8a8cea3d056eaf04c8ff44d985881c6
Derrick McKee <derrick.mckee@gmail.com>
2019-04-01 10:09:23 -0400

    moving fuzzing functionality into utility function

--- src/fosbin-sleuth/python/contexts/binaryutils.py
+++ src/fosbin-sleuth/python/contexts/binaryutils.py
@@ -1,6 +1,11 @@
+import os
 import subprocess
+from concurrent import futures
 
+from .FBLogging import logger
 from .FunctionDescriptor import FunctionDescriptor
+from .IOVec import IOVec
+from .PinRun import PinMessage, PinRun
 
 
 def find_funcs(binary, target=None, ignored_funcs=None):
@@ -29,3 +34,124 @@ def find_funcs(binary, target=None, ignored_funcs=None):
                 location_map[loc] = FunctionDescriptor(binary, name, loc)
     return location_map
 
+
+class FuzzRunDesc:
+    def __init__(self, func_desc, pin_loc, pintool_loc, loader_loc, work_dir, watchdog, fuzz_count):
+        self.func_desc = func_desc
+        self.pin_loc = os.path.abspath(pin_loc)
+        self.pintool_loc = os.path.abspath(pintool_loc)
+        self.loader_loc = os.path.abspath(loader_loc)
+        self.work_dir = os.path.abspath(work_dir)
+        self.watchdog = watchdog
+        self.fuzz_count = fuzz_count
+
+
+def fuzz_one_function(fuzz_desc):
+    func_name = fuzz_desc.func_desc.name
+    target = fuzz_desc.func_desc.location
+    binary = fuzz_desc.func_desc.binary
+    successful_contexts = set()
+
+    if os.path.splitext(binary)[1] == ".so":
+        target = func_name
+
+    run_name = "{}.{}.{}".format(os.path.basename(binary), func_name, target)
+    if fuzz_desc.loader_loc is None:
+        logger.debug("{} target is {}".format(run_name, hex(target)))
+    else:
+        logger.debug("{} target is {}".format(run_name, target))
+    pipe_in = os.path.join(fuzz_desc.work_dir, run_name + ".in")
+    pipe_out = os.path.join(fuzz_desc.work_dir, run_name + ".out")
+    log_out = os.path.join("logs", "fuzz", run_name + ".log")
+    cmd_log = os.path.join("logs", "fuzz", run_name + ".cmd.log")
+    if not os.path.exists(os.path.dirname(log_out)):
+        os.makedirs(os.path.dirname(log_out), exist_ok=True)
+
+    logger.debug("Creating PinRun for {}".format(run_name))
+    pin_run = PinRun(fuzz_desc.pin_loc, fuzz_desc.pintool_loc, binary, fuzz_desc.loader_loc, pipe_in=pipe_in,
+                     pipe_out=pipe_out, log_loc=log_out, cwd=fuzz_desc.work_dir, cmd_log_loc=cmd_log)
+    logger.debug("Done")
+    try:
+        for x in range(fuzz_desc.fuzz_count):
+            try:
+                if not pin_run.is_running():
+                    logger.debug("Starting PinRun for {}".format(run_name))
+                    pin_run.start()
+                    ack_msg = pin_run.send_set_target_cmd(target, fuzz_desc.watchdog)
+                    if ack_msg is None or ack_msg.msgtype != PinMessage.ZMSG_ACK:
+                        raise RuntimeError("Could not set target {}".format(target))
+
+                    resp_msg = pin_run.read_response(timeout=fuzz_desc.watchdog)
+                    if resp_msg is None or resp_msg.msgtype != PinMessage.ZMSG_OK:
+                        raise RuntimeError("Could not set target {}".format(target))
+
+                ack_msg = pin_run.send_reset_cmd(timeout=fuzz_desc.watchdog)
+                if ack_msg is None or ack_msg.msgtype != PinMessage.ZMSG_ACK:
+                    continue
+
+                resp_msg = pin_run.read_response(timeout=fuzz_desc.watchdog)
+                if resp_msg is None or resp_msg.msgtype \
+                        != PinMessage.ZMSG_OK:
+                    continue
+
+                ack_msg = pin_run.send_fuzz_cmd(timeout=fuzz_desc.watchdog)
+                if ack_msg is None or ack_msg.msgtype != PinMessage.ZMSG_ACK:
+                    continue
+                resp_msg = pin_run.read_response(timeout=fuzz_desc.watchdog)
+                if resp_msg is None or resp_msg.msgtype != PinMessage.ZMSG_OK:
+                    continue
+
+                ack_msg = pin_run.send_execute_cmd(timeout=fuzz_desc.watchdog)
+                if ack_msg is None or ack_msg.msgtype != PinMessage.ZMSG_ACK:
+                    continue
+
+                result = pin_run.read_response(timeout=fuzz_desc.watchdog)
+                if result is not None and result.msgtype == PinMessage.ZMSG_OK:
+                    successful_contexts.add(IOVec(result.data))
+                elif result is not None and len(result.data.getbuffer()) > 0:
+                    logger.info("Fuzzing run failed: {}".format(result.data.getvalue()))
+            except TimeoutError as e:
+                logger.exception(str(e))
+                pin_run.stop()
+                continue
+            except AssertionError as e:
+                logger.exception(str(e))
+                pin_run.stop()
+                continue
+
+    except Exception as e:
+        logger.exception("Error for {}: {}".format(run_name, e))
+    finally:
+        logger.info("Finished {}".format(run_name))
+        pin_run.stop()
+        if os.path.exists(pipe_in):
+            os.unlink(pipe_in)
+        if os.path.exists(pipe_out):
+            os.unlink(pipe_out)
+        del pin_run
+        return successful_contexts
+
+
+def fuzz_functions(func_descs, pin_loc, pintool_loc, loader_loc, num_threads, watchdog=5.0, fuzz_count=5,
+                   work_dir=os.path.abspath(os.path.join(os.curdir, "_work"))):
+    fuzz_runs = list()
+    io_vecs_dict = dict()
+
+    if not os.path.exists(work_dir):
+        os.makedirs(work_dir, exist_ok=True)
+
+    for func_desc in func_descs:
+        fuzz_runs.append(FuzzRunDesc(func_desc, pin_loc, pintool_loc, loader_loc, work_dir, watchdog, fuzz_count))
+
+    with futures.ThreadPoolExecutor(max_workers=num_threads) as pool:
+        results = {pool.submit(fuzz_one_function, fuzz_run): fuzz_run for fuzz_run in fuzz_runs}
+        for result in futures.as_completed(results):
+            fuzz_run = results[result]
+            try:
+                io_vecs = result.result()
+                if len(io_vecs) > 0:
+                    io_vecs_dict[fuzz_run] = io_vecs
+            except Exception as e:
+                continue
+
+    return io_vecs_dict
--- src/fosbin-sleuth/python/fuzz-applications.py
+++ src/fosbin-sleuth/python/fuzz-applications.py
@@ -1,143 +1,21 @@
 #!/usr/bin/python3.7
 
-import os
 import argparse
-from contexts import binaryutils
-from contexts.PinRun import PinRun, PinMessage
-from contexts.IOVec import IOVec
-from contexts.FunctionDescriptor import FunctionDescriptor
+import logging
 import multiprocessing
+import os
+import pickle
 import threading
 from concurrent import futures
+
+from contexts import binaryutils
 from contexts.FBLogging import logger
-import logging
-import pickle
 
 fuzz_count = 5
-watchdog = 5.0
-work_dir = os.path.join("_work", "fuzz")
-
-fail_lock = threading.RLock()
-failed_runs = list()
-
-success_count = 0
-success_lock = threading.RLock()
-
-pin_loc = None
-pintool_loc = None
-loader_loc = None
 
 contexts = dict()
 contexts_hashes = dict()
 hash_lock = threading.RLock()
-
-current_jobs = set()
-
-
-def fuzz_one_function(func_desc):
-    global success_count, pin_loc, pintool_loc, loader_loc, contexts, failed_runs, current_jobs
-    func_name = func_desc.name
-    target = func_desc.location
-    binary = func_desc.binary
-
-    if os.path.splitext(binary)[1] == ".so":
-        target = func_name
-
-    run_name = "{}.{}.{}".format(os.path.basename(binary), func_name, target)
-    if loader_loc is None:
-        logger.debug("{} target is {}".format(run_name, hex(target)))
-    else:
-        logger.debug("{} target is {}".format(run_name, target))
-    current_jobs.add(run_name)
-    pipe_in = os.path.join(work_dir, run_name + ".in")
-    pipe_out = os.path.join(work_dir, run_name + ".out")
-    log_out = os.path.join("logs", "fuzz", run_name + ".log")
-    cmd_log = os.path.join("logs", "fuzz", run_name + ".cmd.log")
-    if not os.path.exists(os.path.dirname(log_out)):
-        os.makedirs(os.path.dirname(log_out), exist_ok=True)
-    successful_runs = 0
-
-    if run_name in contexts:
-        successful_contexts = contexts[run_name]
-    else:
-        successful_contexts = set()
-
-    logger.debug("Creating PinRun for {}".format(run_name))
-    pin_run = PinRun(pin_loc, pintool_loc, binary, loader_loc, pipe_in=pipe_in, pipe_out=pipe_out,
-                     log_loc=log_out, cwd=work_dir, cmd_log_loc=cmd_log)
-    logger.debug("Done")
-    try:
-        for x in range(fuzz_count):
-            try:
-                if not pin_run.is_running():
-                    logger.debug("Starting PinRun for {}".format(run_name))
-                    pin_run.start()
-                    ack_msg = pin_run.send_set_target_cmd(target, watchdog)
-                    if ack_msg is None or ack_msg.msgtype != PinMessage.ZMSG_ACK:
-                        raise RuntimeError("Could not set target {}".format(target))
-
-                    resp_msg = pin_run.read_response(timeout=watchdog)
-                    if resp_msg is None or resp_msg.msgtype != PinMessage.ZMSG_OK:
-                        raise RuntimeError("Could not set target {}".format(target))
-
-                ack_msg = pin_run.send_reset_cmd(timeout=watchdog)
-                if ack_msg is None or ack_msg.msgtype != PinMessage.ZMSG_ACK:
-                    continue
-
-                resp_msg = pin_run.read_response(timeout=watchdog)
-                if resp_msg is None or resp_msg.msgtype \
-                        != PinMessage.ZMSG_OK:
-                    continue
-
-                ack_msg = pin_run.send_fuzz_cmd(timeout=watchdog)
-                if ack_msg is None or ack_msg.msgtype != PinMessage.ZMSG_ACK:
-                    continue
-                resp_msg = pin_run.read_response(timeout=watchdog)
-                if resp_msg is None or resp_msg.msgtype != PinMessage.ZMSG_OK:
-                    continue
-
-                ack_msg = pin_run.send_execute_cmd(timeout=watchdog)
-                if ack_msg is None or ack_msg.msgtype != PinMessage.ZMSG_ACK:
-                    continue
-
-                result = pin_run.read_response(timeout=watchdog)
-                if result is not None and result.msgtype == PinMessage.ZMSG_OK:
-                    successful_runs += 1
-                    successful_contexts.add(IOVec(result.data))
-                elif result is not None and len(result.data.getbuffer()) > 0:
-                    logger.info("Fuzzing run failed: {}".format(result.data.getvalue()))
-            except TimeoutError as e:
-                logger.exception(str(e))
-                pin_run.stop()
-                continue
-            except AssertionError as e:
-                logger.exception(str(e))
-                pin_run.stop()
-                continue
-
-        if successful_runs == 0:
-            raise AssertionError("No successful runs")
-        else:
-            success_lock.acquire()
-            success_count += 1
-            func_desc = FunctionDescriptor(binary, func_name, target)
-            contexts[func_desc] = successful_contexts
-            success_lock.release()
-    except Exception as e:
-        fail_lock.acquire()
-        logger.exception("Error for {}: {}".format(run_name, e))
-        if successful_runs == 0:
-            failed_runs.append(run_name)
-        fail_lock.release()
-    finally:
-        logger.info("Finished {}".format(run_name))
-        current_jobs.remove(run_name)
-        pin_run.stop()
-        if os.path.exists(pipe_in):
-            os.unlink(pipe_in)
-        if os.path.exists(pipe_out):
-            os.unlink(pipe_out)
-        del pin_run
 
 
 def hash_contexts(run_name):
@@ -151,7 +29,7 @@ def hash_contexts(run_name):
 
 
 def main():
-    global loader_loc, pintool_loc, pin_loc, contexts, failed_runs, current_jobs, contexts_hashes, fuzz_count
+    global fuzz_count, contexts, contexts_hashes
 
     parser = argparse.ArgumentParser(description="Generate input/output vectors")
     parser.add_argument("-pindir", help="/path/to/pin/dir", required=True)
@@ -196,6 +74,7 @@ def main():
         exit(1)
 
     fuzz_count = results.count
+    loader_loc = None
     if results.ld is not None:
         loader_loc = os.path.abspath(results.ld)
 
@@ -203,8 +82,6 @@ def main():
     pin_loc = os.path.abspath(os.path.join(results.pindir, "pin"))
 
     ignored_funcs = set()
-    if not os.path.exists(work_dir):
-        os.makedirs(work_dir, exist_ok=True)
 
     if results.ignore is not None:
         logger.debug("Reading ignored functions")
@@ -236,14 +113,8 @@ def main():
         logger.debug("done")
 
     context_file = os.path.abspath(results.ctx)
-    if os.path.exists(context_file):
-        logger.debug("Reading previous contexts")
-        with open(context_file, "rb") as file:
-            contexts = pickle.load(file)
-        logger.debug("done")
-    else:
-        if not os.path.exists(os.path.dirname(context_file)):
-            os.makedirs(os.path.dirname(context_file), exist_ok=True)
+    if not os.path.exists(os.path.dirname(context_file)):
+        os.makedirs(os.path.dirname(context_file), exist_ok=True)
 
     args = list()
     logger.debug("Building fuzz target list")
@@ -254,35 +125,21 @@ def main():
     logger.info("Fuzzing {} targets".format(len(args)))
 
     if len(args) > 0:
-        with futures.ThreadPoolExecutor(max_workers=results.threads) as pool:
-            try:
-                pool.map(fuzz_one_function, args)
-            except KeyboardInterrupt:
-                print("Current jobs: {}".format(current_jobs))
-                # exit(0)
+        contexts = binaryutils.fuzz_functions(args, pin_loc, pintool_loc, loader_loc, results.threads,
+                                              fuzz_count=results.count)
 
         with futures.ThreadPoolExecutor(max_workers=results.threads) as pool:
-            try:
-                pool.map(hash_contexts, contexts.keys())
-            except KeyboardInterrupt:
-                print("Current jobs: {}".format(current_jobs))
-                # exit(0)
+            pool.map(hash_contexts, contexts.keys())
+
+        logger.info("{} has {} functions".format(results.bin, func_count))
+        logger.info("Fuzzable functions: {}".format(len(contexts)))
+
+        with open(context_file, "wb") as file:
+            pickle.dump(contexts, file)
 
         with open(hash_file, "wb") as hashes_out:
             pickle.dump(contexts_hashes, hashes_out)
 
-        logger.info("{} has {} functions".format(results.bin, func_count))
-        logger.info("Fuzzable functions: {}".format(success_count))
-        with open(context_file, "wb") as file:
-            pickle.dump(contexts, file)
-
-        failed_count = len(failed_runs)
-        if failed_count + success_count > 0:
-            logger.info("Failed functions: {} ({})".format(failed_count, failed_count / (failed_count + success_count)))
-        if failed_count > 0:
-            logger.info("Failed run selection:")
-            for run in failed_runs[0:min(5, len(failed_runs))]:
-                logger.info("\t{}".format(run))
     else:
         logger.fatal("Could not find any functions to fuzz")
 
