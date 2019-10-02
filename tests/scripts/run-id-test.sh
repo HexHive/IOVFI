f1d06059001bfca8ce4a1cbcdd8ba471244b6564
Derrick McKee <derrick.mckee@gmail.com>
2019-10-02 11:12:35 -0400

    changing test to use only the smallest and largest binaries

--- tests/scripts/run-id-test.sh
+++ tests/scripts/run-id-test.sh
@@ -18,7 +18,8 @@ ID_SCRIPT=$(realpath $(dirname $PINTOOL)/../../../src/fosbin-sleuth/python/Ident
 IGNORE_PATH="$(realpath $(dirname $PINTOOL)/../../../tests/ignored.txt)"
 echo $CURR_DIR
 
-for test in $(find $TESTS -type f -executable); do
+# Smallest 5
+for test in $(find $TESTS -type f -executable -printf "%p \t%k kb\n" | sort -k2n | awk '{ if(count<5) { print $1; } count++; }'); do
   if [ -d "$(basename $test)" ]; then
     echo "Skipping $test"
     continue
@@ -29,9 +30,25 @@ for test in $(find $TESTS -type f -executable); do
   echo "Starting $test: $(date)" >>"$CMD_FILE"
   cmd="$ID_SCRIPT -t $DATA_DIR/tree.bin -pindir $PIN -tool $PINTOOL -b $test -ignore $IGNORE_PATH"
   echo "CMD: $cmd" >>"$CMD_FILE"
-  $cmd
+#  $cmd
   cd $CURR_DIR
+done
 
+# Largest 5
+for test in $(find $TESTS -type f -executable -printf "%p \t%k kb\n" | sort -k2nr | \
+  awk '{ if(count<5) { print $1; } count++; }'); do
+  if [ -d "$(basename $test)" ]; then
+    echo "Skipping $test"
+    continue
+  fi
+  mkdir $(basename $test)
+  cd $(basename $test)
+  echo "Evaluating $test"
+  echo "Starting $test: $(date)" >>"$CMD_FILE"
+  cmd="$ID_SCRIPT -t $DATA_DIR/tree.bin -pindir $PIN -tool $PINTOOL -b $test -ignore $IGNORE_PATH"
+  echo "CMD: $cmd" >>"$CMD_FILE"
+#  $cmd
+  cd $CURR_DIR
 done
 
 echo "Ended at $(date)" >>"$CMD_FILE"
