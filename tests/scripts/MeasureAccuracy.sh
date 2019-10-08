82b1e0d4aad47ea23c6a48e416da86f67ec9ffbb
Derrick McKee <derrick.mckee@gmail.com>
2019-10-08 12:11:18 -0400

    adding script for measuring all accuracies and outputting data to files

--- tests/scripts/MeasureAccuracy.sh
+++ tests/scripts/MeasureAccuracy.sh
@@ -7,7 +7,7 @@ for dir in $(find $ROOT_DIR -mindepth 2 -maxdepth 2 -type d); do
   cd $dir
   for dir2 in $(find . -mindepth 1 -maxdepth 1 -type d); do
     cd $dir2
-    find . -type f -name "guesses.bin" > guesses.txt
+    find . -type f -name "guesses.bin" >guesses.txt
     python3 $ACCURACY_SCRIPT -tree ../tree.bin -o "../$dir2-guesses.bin"
   done
   cd $ROOT_DIR
