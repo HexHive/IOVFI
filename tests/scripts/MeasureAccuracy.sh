a1c361bacd4b0bd84336846a593d0d9c4d9e24e2
Derrick McKee <derrick.mckee@gmail.com>
2019-10-08 12:21:47 -0400

    fixing bugs

--- tests/scripts/MeasureAccuracy.sh
+++ tests/scripts/MeasureAccuracy.sh
@@ -7,8 +7,8 @@ for dir in $(find $ROOT_DIR -mindepth 2 -maxdepth 2 -type d); do
   cd $dir
   for dir2 in $(find . -mindepth 1 -maxdepth 1 -type d); do
     cd $dir2
-    #    find . -type f -name "guesses.bin" >guesses.txt
-    echo "$PWD: python3 $ACCURACY_SCRIPT -tree ../tree.bin -o \"../$dir2-guesses.bin\""
+    find . -type f -name "guesses.bin" >guesses.txt
+    python3 $ACCURACY_SCRIPT -tree ../../tree.bin -o "../../$dir2-guesses.bin"
   done
   cd $ROOT_DIR
 done
