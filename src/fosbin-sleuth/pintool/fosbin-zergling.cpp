5209fe3e7decbe5164fd371ce0698a7488e918a3
Derrick McKee <derrick.mckee@gmail.com>
2019-10-21 16:27:10 -0400

    bug fix

--- src/fosbin-sleuth/pintool/fosbin-zergling.cpp
+++ src/fosbin-sleuth/pintool/fosbin-zergling.cpp
@@ -928,9 +928,9 @@ zerg_cmd_result_t handle_set_target(ZergMessage &zmsg) {
             INS_InsertCall(ins, IPOINT_BEFORE, (AFUNPTR) report_success, IARG_CONTEXT, IARG_THREAD_ID, IARG_END);
         }
     }
-    instrument_sites++;
-    INS_InsertCall(RTN_InsTail(new_target), IPOINT_AFTER, (AFUNPTR) report_success, IARG_CONTEXT,
-                   IARG_THREAD_ID, IARG_END);
+//    instrument_sites++;
+//    INS_InsertCall(RTN_InsTail(new_target), IPOINT_AFTER, (AFUNPTR) report_success, IARG_CONTEXT,
+//                   IARG_THREAD_ID, IARG_END);
 
     last_ins_addr = INS_Address(RTN_InsTail(new_target));
     RTN_Close(new_target);
