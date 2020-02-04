5263bf8c61ca38ab672416f10dc8c8baeb334256
Derrick McKee <derrick.mckee@gmail.com>
2020-02-04 16:13:05 -0500

    adding in system call output

--- src/fosbin-sleuth/pintool/IOVec.cpp
+++ src/fosbin-sleuth/pintool/IOVec.cpp
@@ -12,7 +12,11 @@ std::ostream &operator<<(std::ostream &out, const IOVec &ioVec) {
     out << ioVec.preContext_;
     out << ioVec.postContext_;
     out << ioVec.coverage_;
-    /* TODO: Add in system call output */
+    size_t syscall_size = ioVec.systemCalls_.size();
+    out.write((const char *) &syscall_size, sizeof(syscall_size));
+    for (ADDRINT i : ioVec.systemCalls_) {
+        out.write((const char *) &i, sizeof(ADDRINT));
+    }
 
     return out;
 }
