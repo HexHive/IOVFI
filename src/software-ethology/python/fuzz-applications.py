6286ebaa51bef3ee0dbc12e0dbcb64154cb39d4c
Derrick McKee <derrick.mckee@gmail.com>
2020-04-27 17:58:15 -0400

    trying to improve fuzzing

--- src/software-ethology/python/fuzz-applications.py
+++ src/software-ethology/python/fuzz-applications.py
@@ -195,7 +195,7 @@ def fuzz_one_function(fuzz_desc, io_vec_list, coverage_map, duration, sema, inst
                                 max_coverage = len(coverage)
 
                         logger.debug("IOVec chosen: {}".format(str(io_vec)))
-                        io_vec.random_seed = random.randint()
+                        io_vec.random_seed = random.randint(0, 2147483647)
                         ack_msg = segrind_run.send_set_ctx_cmd(io_vec)
                         if ack_msg and ack_msg.msgtype == SEMsgType.SEMSG_ACK:
                             resp_msg = segrind_run.read_response()
