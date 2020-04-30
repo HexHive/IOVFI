b9d1cd8805518d9f68a7fe1f5c4f40197168ac03
Derrick McKee <derrick.mckee@gmail.com>
2020-04-30 08:06:35 -0400

    adding additional condition for when to test existing IOVecs

--- src/software-ethology/python/fuzz-applications.py
+++ src/software-ethology/python/fuzz-applications.py
@@ -186,7 +186,8 @@ def fuzz_one_function(fuzz_desc, io_vec_list, coverage_map, duration, sema, inst
                 using_external_iovec = False
                 using_internal_iovec = False
                 io_vec = None
-                if len(io_vec_list) > current_iovec_idx and (hit_threshold or time.time() > start_time + duration):
+                if len(io_vec_list) > current_iovec_idx and (
+                        hit_threshold or fuzz_stats.total_io_vecs_created == 0 or time.time() > start_time + duration):
                     while current_iovec_idx < len(io_vec_list):
                         if io_vec_list[current_iovec_idx] not in coverage_map[fuzz_desc.func_desc]:
                             io_vec = io_vec_list[current_iovec_idx]
