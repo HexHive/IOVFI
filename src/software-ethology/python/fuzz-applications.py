650a80f80584562f007cf25dd738bb7340803813
Derrick McKee <derrick.mckee@gmail.com>
2020-04-30 10:48:19 -0400

    only generating tree if we specify name

--- src/software-ethology/python/fuzz-applications.py
+++ src/software-ethology/python/fuzz-applications.py
@@ -8,10 +8,10 @@ import sys
 import time
 
 import contexts.binaryutils as bu
+from contexts.FBDecisionTree import FBDecisionTree
 from contexts.FBLogging import logger
 from contexts.IOVec import IOVec
 from contexts.SEGrindRun import SEGrindRun, SEMsgType
-from contexts.FBDecisionTree import FBDecisionTree
 
 MAX_ATTEMPTS = 25
 WATCHDOG = 3
@@ -408,7 +408,7 @@ def main():
     parser.add_argument('-o', '--out', help="/path/to/output/fuzzing/results", default="out.desc")
     parser.add_argument("-timeout", help='Number of seconds to wait per run', type=int, default=WATCHDOG)
     parser.add_argument('-duration', help='Total number of seconds to fuzz targets', type=int, default=DEFAULT_DURATION)
-    parser.add_argument('-t', '--tree', help="File to output decision tree", default="tree.bin")
+    parser.add_argument('-t', '--tree', help="File to output decision tree")
 
     results = parser.parse_args()
 
@@ -472,9 +472,12 @@ def main():
     out_file = os.path.abspath(results.out)
     if not os.path.exists(os.path.dirname(out_file)):
         os.makedirs(os.path.dirname(out_file), exist_ok=True)
-    tree_file = os.path.abspath(results.tree)
-    if not os.path.exists(os.path.dirname(tree_file)):
-        os.makedirs(os.path.dirname(tree_file), exist_ok=True)
+    if results.tree is not None:
+        tree_file = os.path.abspath(results.tree)
+        if not os.path.exists(os.path.dirname(tree_file)):
+            os.makedirs(os.path.dirname(tree_file), exist_ok=True)
+    else:
+        tree_file = None
 
     args = list()
     logger.debug("Building fuzz target list")
@@ -500,11 +503,12 @@ def main():
         with open(out_file, "wb") as f:
             pickle.dump(fuzz_run_results, f)
 
-        logger.info("Creating tree")
-        decision_tree = FBDecisionTree(out_file)
-        with open(tree_file, 'wb') as f:
-            pickle.dump(decision_tree)
-        logger.info("done")
+        if tree_file:
+            logger.info("Creating tree")
+            decision_tree = FBDecisionTree(out_file)
+            with open(tree_file, 'wb') as f:
+                pickle.dump(decision_tree, f)
+            logger.info("done")
     else:
         logger.fatal("Could not find any functions to fuzz")
 
