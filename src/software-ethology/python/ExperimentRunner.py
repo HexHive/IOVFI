048162c3c52b51f19d1b20a7b1832d3caafa92fe
Derrick McKee <derrick.mckee@gmail.com>
2020-05-05 06:07:27 -0400

    debugging

--- src/software-ethology/python/ExperimentRunner.py
+++ src/software-ethology/python/ExperimentRunner.py
@@ -106,11 +106,11 @@ class Experiment:
         self.start_time = time.time()
         orig_sysout = sys.stdout
         orig_syserr = sys.stderr
-        log_path = os.path.abspath(os.path.join(self.base_dir, "{}.log".format(self.id)))
-        err_path = os.path.abspath(os.path.join(self.base_dir, "{}.err".format(self.id)))
-        self.log('Logging to {}'.format(log_path))
-        sys.stdout = open(log_path, 'w')
-        sys.stderr = open(err_path, 'w')
+        # log_path = os.path.abspath(os.path.join(self.base_dir, "{}.log".format(self.id)))
+        # err_path = os.path.abspath(os.path.join(self.base_dir, "{}.err".format(self.id)))
+        # self.log('Logging to {}'.format(log_path))
+        # sys.stdout = open(log_path, 'w')
+        # sys.stderr = open(err_path, 'w')
         try:
             for tree in self.trees:
                 self.log("Starting evaluation of {}".format(tree['dest']))
@@ -129,8 +129,8 @@ class Experiment:
                 else:
                     self.log("ERROR: Tree creation failed for {}".format(tree['dest']))
         finally:
-            sys.stdout.close()
-            sys.stderr.close()
+            # sys.stdout.close()
+            # sys.stderr.close()
             sys.stdout = orig_sysout
             sys.stderr = orig_syserr
 
