4b7b8aef6b9b479a29211f1a5fcbb45dd34e4123
Derrick McKee <derrick.mckee@gmail.com>
2020-05-14 06:38:15 -0400

    adding log level to experiment description

--- src/software-ethology/python/ExperimentRunner.py
+++ src/software-ethology/python/ExperimentRunner.py
@@ -15,7 +15,8 @@ class Directory:
 
 
 class Experiment:
-    def __init__(self, id, timeout, trees, eval_dirs, eval_bins, base_dir, se_dir, valgrind, so_loader, duration):
+    def __init__(self, id, timeout, trees, eval_dirs, eval_bins, base_dir, se_dir, valgrind, so_loader, duration,
+                 log_level):
         if not os.path.exists(valgrind):
             raise FileNotFoundError(valgrind)
         if trees is None or len(trees) == 0:
@@ -43,6 +44,7 @@ class Experiment:
         self.executed_commands = 0
         self.timeout = timeout
         self.duration = duration
+        self.log_level = log_level
 
     def init(self):
         self.ignore = os.path.join(self.se_dir, "tests", "ignored.txt")
@@ -111,9 +113,9 @@ class Experiment:
             self.log("Creating tree {} from source {}".format(tree['dest'], tree['src_bin']))
             if not dry_run and not os.path.exists(tree['src_bin']):
                 raise AssertionError("Tree source {} does not exist".format(tree['src_bin']))
-            cmd = "python3 {} -valgrind {} -bin {} -ignore {} -t {} -timeout {} -loader {} -duration {}".format(
+            cmd = "python3 {} -valgrind {} -bin {} -ignore {} -t {} -timeout {} -loader {} -duration {} -loglevel {}".format(
                 os.path.join(self.se_dir, "src", "software-ethology", "python", "fuzz-applications.py"), self.valgrind,
-                tree['src_bin'], self.ignore, tree['dest'], self.timeout, self.so_loader, self.duration)
+                tree['src_bin'], self.ignore, tree['dest'], self.timeout, self.so_loader, self.duration, self.log_level)
             self.execute_command(cmd, dry_run=dry_run)
         else:
             self.log("{} already exists...skipping".format(tree['dest']))
@@ -124,10 +126,10 @@ class Experiment:
 
     def identify_functions(self, tree_path, binary_path, guess_path, dry_run=True):
         self.change_directory(os.path.dirname(guess_path), dry_run)
-        cmd = "python3 {} -valgrind {} -b {} -ignore {} -t {} -guesses {} -timeout {} -loader {}".format(
+        cmd = "python3 {} -valgrind {} -b {} -ignore {} -t {} -guesses {} -timeout {} -loader {} -loglevel {}".format(
             os.path.join(self.se_dir, "src", "software-ethology", "python", "IdentifyFunction.py"), self.valgrind,
             os.path.abspath(binary_path), self.ignore, os.path.abspath(tree_path), guess_path, self.timeout,
-            self.so_loader)
+            self.so_loader, self.log_level)
         self.execute_command(cmd, dry_run=dry_run)
 
     def compute_accuracy(self, tree_path, guess_path, output_path, dry_run=True):
