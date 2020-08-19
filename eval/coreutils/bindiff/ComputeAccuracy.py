854285435d8bb0484e64b72787c08987f379124b
Derrick McKee <derrick.mckee@gmail.com>
2020-08-19 17:27:11 -0400

    adding bindiff accuracy script

---
+++ eval/coreutils/bindiff/ComputeAccuracy.py
@@ -0,0 +1,73 @@
+from sklearn.metrics import f1_score
+import argparse
+import re
+
+def compute_accuracy(bindiff, primary, secondary):
+    functionMatchRegex = "([0-9A-Fa-f]+)\s+([" \
+                         "0-9A-Fa-f]+)\s+\d+\s+0\.\d+\s+0\.\d+\s+0\.\d+\s+\d" \
+                         "+\s+\d+\sfunction:.+\"(\w+)\"\s+\"(\w+)\""
+    functionMatch = re.compile(functionMatchRegex)
+    unmatchedRegex = "([0-9A-Fa-f]+)\s+\d+\s+\d+\s+(\w+)"
+    unmatchedMatch = re.compile(unmatchedRegex)
+
+    reading_unmatched_primary = False
+    reading_unmatched_secondary = False
+
+    primary_syms = set()
+    secondary_syms = set()
+    predictions = list()
+    truths = list()
+    unmatched_secondary = list()
+
+    unknown = "@@UNKNOWN@@"
+
+    with open(bindiff, 'r') as f:
+        for line in f.readlines():
+            match = functionMatch.match(line)
+            if match:
+                primary_syms.add(match.group(3).strip())
+                secondary_syms.add(match.group(4).strip())
+                predictions.append(match.group(4).strip())
+                truths.append(match.group(3).strip())
+            elif re.search("unmatched primary", line):
+                reading_unmatched_primary = True
+                reading_unmatched_secondary = False
+            elif re.search("unmatched secondary", line):
+                reading_unmatched_secondary = True
+                reading_unmatched_primary = False
+            elif reading_unmatched_primary:
+                match = unmatchedMatch.match(line)
+                if match:
+                    primary_syms.add(match.group(2).strip())
+            elif reading_unmatched_secondary:
+                match = unmatchedMatch.match(line)
+                if match:
+                    print("{}: {}".format(match.group(2).strip(), line))
+                    secondary_syms.add(match.group(2).strip())
+                    unmatched_secondary.append(match.group(2).strip())
+
+            for unmatched_sym in unmatched_secondary:
+                print(unmatched_sym)
+                predictions.append(unknown)
+                if unmatched_sym in primary_syms:
+                    truths.append(unmatched_sym)
+                else:
+                    truths.append(unknown)
+
+        for i in range(len(truths)):
+            print("{} {}".format(truths[i], predictions[i]))
+
+        print(f1_score(truths, predictions, average='micro'))
+
+def main():
+    parser = argparse.ArgumentParser(description="Computes BinDiff Accuracy")
+    parser.add_argument('-p', '--primary', required=True)
+    parser.add_argument('-s', '--secondary', required=True)
+    parser.add_argument('-b', '--bindiffLog', required=True)
+    args = parser.parse_args()
+
+    compute_accuracy(args.bindiffLog, args.primary, args.secondary)
+
+
+if __name__ == "__main__":
+    main()
