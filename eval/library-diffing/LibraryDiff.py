756bccf63b26518dbaf0449d0905ce15fdeaf60e
Derrick McKee <derrick.mckee@gmail.com>
2020-12-04 11:47:57 -0500

    fixing line parse

--- eval/library-diffing/LibraryDiff.py
+++ eval/library-diffing/LibraryDiff.py
@@ -18,7 +18,7 @@ def get_git_diffs(version1, version2):
     cmd = ['git', 'diff', version1, version2, '--stat']
     diff_result = subprocess.run(cmd, stdout=subprocess.PIPE)
 
-    for line in diff_result.stdout.decode('utf-8').readline():
+    for line in diff_result.stdout.readline().decode('utf-8'):
         stat_match = stat_regex.match(line)
         print(line)
         if stat_match:
