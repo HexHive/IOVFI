7526f3504d49c1bc004a57206080f1e6b6896ce2
Derrick McKee <derrick.mckee@gmail.com>
2020-12-04 11:03:48 -0500

    fixing syntax

--- eval/library-diffing/LibraryDiff.py
+++ eval/library-diffing/LibraryDiff.py
@@ -13,9 +13,7 @@ def get_git_diffs(version1, version2):
     total_adds = 0
     total_dels = 0
 
-    diff_result = subprocess.run(['find', '.', '-name', '\"*.[c,h]\"',
-                                  '-exec', 'git', 'diff', version1, version2,
-                                  '--stat', '--', '{}', '\\;'],
+    diff_result = subprocess.run(['find', '.', '-name', '\"*.[c,h]\"', '-exec', 'git', 'diff', version1, version2, '--stat', '--', '{}', '\\;'],
                                  stdout=subprocess.PIPE)
 
     for line in diff_result.stdout.decode('utf-8'):
